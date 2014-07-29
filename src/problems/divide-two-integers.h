c7ca1370042ddccf548c34a25dc7743382f92d3d
jeswang <wangyi724@gmail.com>
2014-07-29 11:50:56 +0800

    sovle divide-two-integers

--- src/problems/divide-two-integers.h
+++ src/problems/divide-two-integers.h
@@ -12,7 +12,54 @@ Divide two integers without using multiplication, division and mod operator.
 
 class Solution {
 public:
-    int divide(int dividend, int divisor) {
+    int divide(int sdividend, int sdivisor) {
+        unsigned int dividend = sdividend;
+        unsigned int divisor = sdivisor;
         
+        if(divisor == 1){
+            return dividend;
+        }
+        
+        int count = 0;
+        bool flag = 0;
+        if(sdividend < 0) {
+            flag = !flag;
+            dividend = -sdividend;
+        }
+        
+        if(sdivisor < 0) {
+            flag = !flag;
+            divisor = -sdivisor;
+        }
+        
+        while(dividend >= divisor) {
+            count++;
+            dividend = dividend - divisor;
+            
+            int bigCount = 2;
+            unsigned int target = divisor<<1;
+            
+            if (target < divisor) {
+                continue;
+            }
+            
+            while(dividend >= target) {
+                count += bigCount;
+                dividend = dividend - target;
+                
+                bigCount = bigCount << 1;
+                target = target << 1;
+            }
+        }
+        
+        if(flag){
+            count = -count;
+        }
+        
+        return count;
+    }
+    
+    void run() {
+        cout<<divide(-2147483648, -2147483648);
     }
 };

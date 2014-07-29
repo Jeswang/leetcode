950997baba0b21f1741738a426503c4a9f409521
jeswang <wangyi724@gmail.com>
2014-07-29 13:31:48 +0800

    solve decode-ways flatten-binary-tree-to-linked-list

--- src/problems/decode-ways.h
+++ src/problems/decode-ways.h
@@ -25,7 +25,79 @@ The number of ways decoding "12" is 2.
 
 class Solution {
 public:
+    map<string, int> replaceMap = {
+        {"1", 'a'},
+        {"2", 'b'},
+        {"3", 'c'},
+        {"4", 'd'},
+        {"5", 'e'},
+        {"6", 'f'},
+        {"7", 'g'},
+        {"8", 'h'},
+        {"9", 'i'},
+        {"10", 'j'},
+        {"11", 'k'},
+        {"12", 'l'},
+        {"13", 'm'},
+        {"14", 'n'},
+        {"15", 'o'},
+        {"16", 'p'},
+        {"17", 'q'},
+        {"18", 'r'},
+        {"19", 's'},
+        {"20", 't'},
+        {"21", 'u'},
+        {"22", 'v'},
+        {"23", 'w'},
+        {"24", 'x'},
+        {"25", 'y'},
+        {"26", 'z'},
+    };
+    
+    map<string, int> alreadyCount;
+    
     int numDecodings(string s) {
+        int len = (int)s.length();
+        if (len == 0) {
+            return 0;
+        }
+        else {
+            return realNumDecodings(s);
+        }
+    }
+    
+    int realNumDecodings(string s) {
         
+        int count = 0;
+        int len = (int)s.length();
+        
+        if (len == 0) {
+            return 1;
+        }
+        
+        if (alreadyCount.find(s) != alreadyCount.end()) {
+            return alreadyCount[s];
+        }
+        
+        if(len>=1){
+            string one = s.substr(0, 1);
+            if (replaceMap.find(one) != replaceMap.end()) {
+                count += realNumDecodings(s.substr(1, len-1));
+            }
+        }
+        
+        if (len >= 2) {
+            string two = s.substr(0, 2);
+            if (replaceMap.find(two) != replaceMap.end()) {
+                count += realNumDecodings(s.substr(2, len-2));
+            }
+        }
+        
+        alreadyCount[s] = count;
+        return count;
+    }
+    
+    void run() {
+        cout<<numDecodings("226");
     }
 };
--- src/problems/flatten-binary-tree-to-linked-list.h
+++ src/problems/flatten-binary-tree-to-linked-list.h
@@ -46,9 +46,55 @@ If you notice carefully in the flattened tree, each node's right child points to
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+//TODO: runtime error?
+
 class Solution {
 public:
     void flatten(TreeNode *root) {
+        flattenWithReturn(root);
+    }
+    /**
+     *  faltten a tree
+     *
+     *  @param root root
+     *
+     *  @return the last node
+     */
+    TreeNode *flattenWithReturn(TreeNode *root) {
+        if (root == NULL) {
+            return NULL;
+        }
         
+        TreeNode *leftLastNode = NULL, *rightLastNode = NULL, *left, *right;
+        
+        left = root->left;
+        right = root->right;
+        
+        if (left != NULL) {
+            leftLastNode = flattenWithReturn(left);
+            root->right = left;
+            root->left = NULL;
+        }
+        else {
+            leftLastNode = root;
+        }
+        
+        if (right != NULL) {
+            rightLastNode = flattenWithReturn(right);
+            leftLastNode->right = right;
+            return rightLastNode;
+        }
+        else {
+            return leftLastNode;
+        }
+        
+    }
+    
+    void run(){
+        string treeString = "1 2 3 5 # 4";
+        TreeNode *tree = treeFromString(treeString);
+        flatten(tree);
+        cout<<stringFromTree(tree);
     }
 };

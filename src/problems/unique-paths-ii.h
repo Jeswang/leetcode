9407b79fc6a33f475b429913007da9de5bb828c9
jeswang <wangyi724@gmail.com>
2014-07-30 16:51:34 +0800

    solve Unique Paths II

--- src/problems/unique-paths-ii.h
+++ src/problems/unique-paths-ii.h
@@ -21,9 +21,57 @@ The total number of unique paths is 2.
 Note: m and n will be at most 100.
 */
 
+#define foreach(container,it) for(auto it = (container).begin();it!=(container).end();++it)
+#define FD(X, Y) (X.find(Y)!=X.end())
+#define SIZE(A) ((int)A.size())
+#define PB(X) push_back(X)
+
 class Solution {
 public:
+    vector<vector<int> > obstacleGrid_;
+    map<string, int> checked;
+    int total;
+    int m,n;
     int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {
+        obstacleGrid_ = obstacleGrid;
+        total = 0;
+        n = SIZE(obstacleGrid);
+        if (n == 0) {
+            return 0;
+        }
+        else {
+        }
+        m = SIZE(obstacleGrid[0]);
         
+        if (m>0 && n>0 && obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+        
+        total = goFrom(0, 0);
+        return total;
+    }
+    
+    int goFrom(int x, int y) {
+        string key = to_string(x)+"_"+to_string(y);
+        
+        if (FD(checked, key)) {
+            return checked[key];
+        }
+        
+        int count = 0;
+        if (x == m-1 && y == n-1) {
+            return 1;
+        }
+        else {
+            if (x <= m-2 && obstacleGrid_[y][x+1]!=1) {
+                count += goFrom(x+1, y);
+            }
+            if (y <= n-2 && obstacleGrid_[y+1][x]!=1) {
+                count += goFrom(x, y+1);
+            }
+        }
+        
+        checked[key] = count;
+        return count;
     }
 };

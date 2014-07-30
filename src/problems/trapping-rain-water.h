a4661da075a9063e00f6271ceeed588500a25c49
jeswang <wangyi724@gmail.com>
2014-07-30 16:03:52 +0800

    sovle trapping-rain-water

--- src/problems/trapping-rain-water.h
+++ src/problems/trapping-rain-water.h
@@ -18,6 +18,57 @@ The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In th
 class Solution {
 public:
     int trap(int A[], int n) {
+        if (n<3) {
+            return 0;
+        }
+        int low = -1;
+        int acculate = 0;
+        int total = 0;
+        for (int i=0; i<n; i++) {
+            if (low == -1) {
+                low = A[i];
+                continue;
+            }
+            
+            if (A[i] >= low) {
+                total += acculate;
+                acculate = 0;
+                low = A[i];
+                
+            }
+            else {
+                acculate += (low - A[i]);
+            }
+            
+        }
         
+        int high = low;
+
+        low = -1;
+        acculate = 0;
+        
+        for (int i=n-1; i>=0; i--) {
+
+            if (low == -1) {
+                low = A[i];
+            }
+            else if (A[i] >= low) {
+                total += acculate;
+                acculate = 0;
+                low = A[i];
+            }
+            else {
+                acculate += (low - A[i]);
+            }
+            
+            if (A[i] == high) {
+                break;
+            }
+        }
+        return total;
+    }
+    void run() {
+        int a[3] = {2, 0, 2};
+        cout<<trap(a, 3);
     }
 };

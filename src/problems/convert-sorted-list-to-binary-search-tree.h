8f3b5cb07953df265f6d68484655ddd665c25f41
Jesse Wang <wangyi724@gmail.com>
2014-08-21 00:09:13 +0800

    solve Convert Sorted List to Binary Search Tree

--- src/problems/convert-sorted-list-to-binary-search-tree.h
+++ src/problems/convert-sorted-list-to-binary-search-tree.h
@@ -29,6 +29,40 @@ Given a singly linked list where elements are sorted in ascending order, convert
 class Solution {
 public:
     TreeNode *sortedListToBST(ListNode *head) {
+        int len = 0;
+        ListNode* pointer = head;
+        while(pointer!=NULL) {
+            len++;
+            pointer = pointer->next;
+        }
+        return realSortedListToBST(head, len);
+    }
+    
+    TreeNode *realSortedListToBST(ListNode *head, int len) {
+        if (len == 0) {
+            return NULL;
+        }
         
+        if (len == 1) {
+            return new TreeNode(head->val);
+        }
+        
+        ListNode* pointer = head;
+        int half = (len-1)/2;
+        while(half > 0) {
+            half--;
+            pointer = pointer->next;
+        }
+        
+        TreeNode* newHead = new TreeNode(pointer->val);
+        newHead->left = realSortedListToBST(head, (len-1)/2);
+        newHead->right = realSortedListToBST(pointer->next, len-1-(len-1)/2);
+        
+        return newHead;
+    }
+    void run() {
+        ListNode* head = new ListNode(1);
+        head->next = new ListNode(3);
+        sortedListToBST(head);
     }
 };

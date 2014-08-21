a7fd56eedd87c9f804b433549990a383283c5e95
jeswang <wangyi724@gmail.com>
2014-08-21 10:15:24 +0800

    solve zigzag-conversion

--- src/problems/zigzag-conversion.h
+++ src/problems/zigzag-conversion.h
@@ -5,26 +5,56 @@
 //
 
 /*
-Description:
-
-The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
-
-P   A   H   N
-A P L S I I G
-Y   I   R
-
-
-And then read line by line: "PAHNAPLSIIGYIR"
-
-Write the code that will take a string and make this conversion given a number of rows:
-
-string convert(string text, int nRows);
-convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
-*/
+ Description:
+ 
+ The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+ 
+ P   A   H   N
+ A P L S I I G
+ Y   I   R
+ 
+ 
+ And then read line by line: "PAHNAPLSIIGYIR"
+ 
+ Write the code that will take a string and make this conversion given a number of rows:
+ 
+ string convert(string text, int nRows);
+ convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
+ */
 
 class Solution {
 public:
     string convert(string s, int nRows) {
+        if (nRows == 1) {
+            return s;
+        }
+        string res;
+        vector<string> vs(nRows, "");
+        int i = 0;
+        int j = 0;
+        int step = 1;
+        unsigned long len = s.length();
+        while(j < len) {
+            if(i == 0) {
+                step = 1;
+            }
+            else if(i == nRows-1) {
+                step = -1;
+            }
+            
+            vs[i]+=s[j];
+            
+            j++;
+            
+            i += step;
+        }
         
+        for(int k = 0; k < nRows; k ++)
+            res += vs[k];
+        
+        return res;
+    }
+    void run() {
+        cout<<convert("AB", 1);
     }
 };

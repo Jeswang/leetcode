1ef0a8e2b8ad7b20df97e803a012a19ca605dca6
jeswang <wangyi724@gmail.com>
2014-08-27 17:05:44 +0800

    solve anagrams
    https://oj.leetcode.com/discuss/4480/solved-it-with-log-time-space-there
    -better-solution-accepted

--- src/problems/anagrams.h
+++ src/problems/anagrams.h
@@ -13,6 +13,17 @@ Note: All inputs will be in lower-case.
 class Solution {
 public:
     vector<string> anagrams(vector<string> &strs) {
-        
+        vector<string> str;
+        map<string, vector<string> > mps;
+        for(string s:strs) {
+            string tmp = s;
+            sort(s.begin(), s.end());
+            mps[s].push_back(tmp);
+        }
+        for(auto mp:mps) {
+            if(mp.second.size()>1)
+                str.insert(str.end(), mp.second.begin(), mp.second.end());
+        }
+        return str;
     }
 };

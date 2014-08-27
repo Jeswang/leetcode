e5259c83b1c3a7b0124733ab4e14374fa68279f2
jeswang <wangyi724@gmail.com>
2014-08-27 18:43:51 +0800

    solve sqrtx

--- src/problems/sqrtx.h
+++ src/problems/sqrtx.h
@@ -12,7 +12,18 @@ Compute and return the square root of x.
 
 class Solution {
 public:
+    
     int sqrt(int x) {
-        
+        long long ans = 0l;
+        long long bit = 1l << 16;
+        while(bit > 0) {
+            ans |= bit;
+            if (ans * ans > x) {
+                ans ^= bit;
+            }
+            bit >>= 1;
+        }
+        return (int)ans;
     }
+    
 };
